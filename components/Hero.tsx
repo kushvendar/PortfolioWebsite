c914ca355ea730705896a4e1ce273c67c8318072
Kush <jangidkush1234@gmail.com>
2024-05-30 16:12:29 +0530

    framer motion loaded

---
+++ components/Hero.tsx
@@ -0,0 +1,8 @@
+
+export const Hero=()=>{
+    return (
+        <div className="pb-20 pt-36">
+            Hero 
+        </div>
+    )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import { DEFAULT_CIPHERS } from "tls";
 
 const config: Config = {
   content: [
@@ -12,7 +13,14 @@ const config: Config = {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        
       },
+      colors:{
+        black:{
+          DEFAULT:'#000',
+        100:'#000319'
+        }
+      }
     },
   },
   plugins: [],
