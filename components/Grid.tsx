f50b8b58025b6651792ca4d7f714d1772d0348fe
Kush <jangidkush1234@gmail.com>
2024-06-02 23:06:27 +0530

    elements added

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,11 +1,22 @@
-    import React from 'react'
-    import { BentoGrid } from './ui/BentoGrid'
+import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { gridItems } from '@/data'
 
     export const Grid = () => {
+
     return (
         <section id='about'>            
         <BentoGrid>
-            
+        {gridItems.map((item,i)=>(
+            <BentoGridItem 
+            className=''
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+
+            />
+        ))}
         </BentoGrid>
         </section>
     )
