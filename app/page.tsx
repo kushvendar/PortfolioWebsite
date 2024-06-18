104134deed88586746ad1c42e202993b39ec487d
Kush <jangidkush1234@gmail.com>
2024-06-18 22:08:46 +0530

    added testimonial

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Client } from "@/components/Client";
 import { Grid } from "@/components/Grid";
 import { Hero } from "@/components/Hero";
 import { RecentProjects } from "@/components/RecentProjects";
@@ -14,6 +15,7 @@ export default function Home() {
        <Hero/>
        <Grid/>
        <RecentProjects/>
+       <Client/>
     </div>
     </main>
   );
