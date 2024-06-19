478f22c4e409c3d28d4d424ba7a7d8ef72501b9c
Kush <jangidkush1234@gmail.com>
2024-06-19 18:03:51 +0530

    finishing the site

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { Client } from "@/components/Client";
+import { Experience } from "@/components/Experience";
 import { Grid } from "@/components/Grid";
 import { Hero } from "@/components/Hero";
 import { RecentProjects } from "@/components/RecentProjects";
@@ -16,6 +17,7 @@ export default function Home() {
        <Grid/>
        <RecentProjects/>
        <Client/>
+       <Experience/>
     </div>
     </main>
   );
